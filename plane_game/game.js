fea19386d4024681ac597d02f0fd3d40ddaf627a
mario <mario55555@cox.net>
2019-06-01 17:09:27 -0400

    Move plane_game into a separate directory

---
+++ plane_game/game.js
@@ -0,0 +1,14 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 1000,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: [GameScene]
+};
+var game = new Phaser.Game(config);
