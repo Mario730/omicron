a65dbd16e793de39625b4a6f78bd19156274c67d
mario <mario55555@cox.net>
2019-06-06 23:07:50 -0700

    Changed EnvironmentObject to sprite instead of image, added some background

--- dungeon_crawler/level1.js
+++ dungeon_crawler/level1.js
@@ -4,20 +4,20 @@ var left;
 var down;
 var right;
 
-class EnvironmentObject extends Phaser.GameObjects.Image {
+class EnvironmentObject extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, frame) {
         super(scene, x, y, 'tilesheet', frame);
         this.scene = scene;
         this.setOrigin(0);
         this.setScale(2);
         this.scene.add.existing(this);
-        this.scene.physics.world.enable(this, Phaser.STATIC_BODY);
     }
 }
 
 class Wall extends EnvironmentObject {
     constructor(scene, x, y, frame) {
         super(scene, x, y, frame);
+        this.scene.physics.world.enable(this, Phaser.STATIC_BODY);
         this.body.setImmovable(true);
         this.body.syncBounds = true;
         this.scene.physics.add.collider(this, hero);
@@ -53,6 +53,10 @@ class SceneMain extends Phaser.Scene {
         this.load.image('1hero2', 'assets/Character_animation/priests_idle/priest2/v1/priest2_v1_2.png');
         this.load.image('1hero3', 'assets/Character_animation/priests_idle/priest2/v1/priest2_v1_3.png');
         this.load.image('1hero4', 'assets/Character_animation/priests_idle/priest2/v1/priest2_v1_4.png');
+        this.load.image('sidetorch1', 'assets/items_and_trap_animation/torch/side_torch_1.png');
+        this.load.image('sidetorch2', 'assets/items_and_trap_animation/torch/side_torch_2.png');
+        this.load.image('sidetorch3', 'assets/items_and_trap_animation/torch/side_torch_3.png');
+        this.load.image('sidetorch4', 'assets/items_and_trap_animation/torch/side_torch_4.png');
     }
 
     create() {
@@ -61,7 +65,7 @@ class SceneMain extends Phaser.Scene {
         down = this.input.keyboard.addKey('S');
         right = this.input.keyboard.addKey('D');
         this.anims.create({
-            key: '1hero-idle',
+            key: '1hero',
             frames: [
                 { key: '1hero1'},
                 { key: '1hero2'},
@@ -71,17 +75,30 @@ class SceneMain extends Phaser.Scene {
             frameRate: 8,
             repeat: -1
         });
+        this.anims.create({
+            key: 'sidetorch',
+            frames: [
+                { key: 'sidetorch1'},
+                { key: 'sidetorch2'},
+                { key: 'sidetorch3'},
+                { key: 'sidetorch4'}
+            ],
+            frameRate: 8,
+            repeat: -1
+        });
         this.add.tileSprite(16*32, 8*32, 32*32, 16*32, 'tilesheet', 78).setScale(2);
         new EnvironmentObject(this, 15*32, 7*32, 11);
+        new EnvironmentObject(this, 15*32, 7*32, 64);
         new EnvironmentObject(this, 16*32, 7*32, 12);
         new EnvironmentObject(this, 17*32, 7*32, 14);
         new EnvironmentObject(this, 15*32, 8*32, 21);
+        new EnvironmentObject(this, 15*32, 8*32, 91).play('sidetorch');
         new EnvironmentObject(this, 16*32, 8*32, 22);
         new EnvironmentObject(this, 17*32, 8*32, 24);
         new EnvironmentObject(this, 15*32, 9*32, 31);
         new EnvironmentObject(this, 16*32, 9*32, 32);
         new EnvironmentObject(this, 17*32, 9*32, 34);
-        hero = new Hero(this, 16*32, 8*32, '1hero1').setScale(2).play('1hero-idle');
+        hero = new Hero(this, 16*32, 8*32, '1hero1').play('1hero');
         new Wall(this, 14*32, 6*32, 0);
         new Wall(this, 15*32, 6*32, 1);
         new Wall(this, 16*32, 6*32, 2);
@@ -104,19 +121,22 @@ class SceneMain extends Phaser.Scene {
         if (up.isDown) {
             hero.body.setVelocityY(-64);
         }
-        else if (left.isDown) {
+        if (left.isDown) {
             hero.setFlipX(true);
             hero.body.setVelocityX(-64);
         }
-        else if (down.isDown) {
+        if (down.isDown) {
             hero.body.setVelocityY(64);
         }
-        else if (right.isDown) {
+        if (right.isDown) {
             hero.setFlipX(false);
             hero.body.setVelocityX(64);
         }
-        else {
-            hero.body.setVelocity(0, 0);
+        if (up.isUp && down.isUp) {
+            hero.body.setVelocityY(0);
+        }
+        if (left.isUp && right.isUp) {
+            hero.body.setVelocityX(0);
         }
     }
 
